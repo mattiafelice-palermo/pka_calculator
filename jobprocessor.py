58a99f634651f72233a69651be4773500525a620
Mattia Felice Palermo <mattiafelice.palermo@gmail.com>
2021-11-30 14:29:40 +0100

    Type checking and hinting
    Added type checking for maxcores and cores_per_job variables, and added type hinting for all variables in jobprocessor.py.

--- jobprocessor.py
+++ jobprocessor.py
@@ -9,35 +9,51 @@ Created on Fri Nov 19 12:40:18 2021
 import queue
 from multiprocessing import Process, Queue
 import os
+from typing import Callable, List
 
 
 class JobProcessor:
-    def __init__(self, jobs_list, maxcores=None, cores_per_job=1):
+    def __init__(
+        self, jobs_list: List[Callable], maxcores: int = -1, cores_per_job: int = 1
+    ):
 
         # Set maximum total number of cores used. If user does not provide it,
         # just deduce from os settings
-        self.maxcores = maxcores
 
-        if maxcores is None:
+        self.maxcores: int
+
+        if maxcores == -1:
             self.maxcores = len(os.sched_getaffinity(0))
+        else:
+            self.maxcores = maxcores
 
-        self.cores_per_job = cores_per_job  # number of cores used per job
+        self.cores_per_job: int = cores_per_job  # number of cores used per job
+
+        if not all((isinstance(self.maxcores, int), self.maxcores > 0)):
+            raise TypeError("maxcores must be a positive integer")
+        if not all((isinstance(cores_per_job, int), cores_per_job > 0)):
+            raise TypeError("cores_per_job must be a positive integer")
 
         # Create job queue
-        self.jobs = []  # list of jobs to create jobs queue and track workers
-        self.jobs_queue = Queue()
-        [self.jobs_queue.put_nowait(job) for job in jobs_list]
+        self.workers: List[
+            Process
+        ] = []  # list of functions (jobs) to create jobs queue
+        self.jobs_queue: Queue = Queue()
 
-        self.number_of_jobs = self.jobs_queue.qsize()
+        for job in jobs_list:
+            self.jobs_queue.put_nowait(job)
+
+        self.number_of_jobs: int = self.jobs_queue.qsize()
 
         print(
-            f"Running {self.number_of_jobs} jobs on {self.maxcores} cores, {self.cores_per_job} cores per job"
+            f"Running {self.number_of_jobs} jobs on {self.maxcores} cores,"
+            f"{self.cores_per_job} cores per job"
         )
 
-        self.running_jobs = Queue()  # queue for *counting* running jobs
-        self._results_queue = Queue()  # queue containing jobs results
+        self.running_jobs: Queue = Queue()  # queue for *counting* running jobs
+        self._results_queue: Queue = Queue()  # queue containing jobs results
 
-    def _job_completion_tracker(self, user_function):
+    def _job_completion_tracker(self, user_function: Callable) -> Callable:
         """
         Take user function and decorate it by setting the number of cores
         available to the jobs, gather the results in a common queue and update
@@ -64,29 +80,35 @@ class JobProcessor:
             os.environ["OMP_NUM_THREADS"] = str(
                 self.cores_per_job
             )  # set maximum cores per job
-            result = user_function()  # run user function and catch the output
+            result: object = user_function()  # run function and catch output
             self._results_queue.put(result)  # store the result in queue
-            counter = self.running_jobs.get_nowait()  # update number of running jobs
+
+            # The logic of the following line is broken - the counter does not
+            # work but it accomplishes updating the number of running jobs,
+            # so for now it's fine
+            counter: int = (
+                self.running_jobs.get_nowait()
+            )  # update number of running jobs
             print(f"Job {counter}/{self.number_of_jobs} completed")
 
         return decorated_function
 
-    def run(self):
+    def run(self) -> List:
 
-        job_counter = 0
+        job_counter: int = 0
 
         while True:
-            used_cores = (
+            used_cores: int = (
                 self.cores_per_job * self.running_jobs.qsize()
             )  # cores currently in use
 
             # Check if adding a new job would exceed the available num of cores
             if used_cores + self.cores_per_job <= self.maxcores:
                 try:
-                    job = self.jobs_queue.get_nowait()  # get job from queue
-                    decorated_job = self._job_completion_tracker(job)
-                    worker = Process(target=decorated_job, args=())
-                    self.jobs.append(worker)
+                    job: Callable = self.jobs_queue.get_nowait()  # get job from queue
+                    decorated_job: Callable = self._job_completion_tracker(job)
+                    worker: Process = Process(target=decorated_job, args=())
+                    self.workers.append(worker)
                     worker.start()
                     job_counter += 1
                     print(f"Starting job {job_counter}")
@@ -101,10 +123,10 @@ class JobProcessor:
                     print("An exception has been caught.")
                     print(error)
 
-        for worker in self.jobs:
+        for worker in self.workers:
             worker.join()
 
-        dump = []
+        dump: List = []
 
         for _ in range(self.number_of_jobs):
             dump.append(self._results_queue.get_nowait())
